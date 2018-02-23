ec5e8ee6927c7adadc22b8d6e38ab1e4cfc1f3b7
Viacheslav Takhtuev <viacheslav.takhtuev@electrolux.com>
2018-02-24 00:33:55 +0300

    Fix argument with non-existent path

--- eel.py
+++ eel.py
@@ -6,7 +6,7 @@ import argparse
 
 def get_path():
     parser = argparse.ArgumentParser()
-    parser.add_argument("path", help="path with target files")
+    parser.add_argument("path", help="folder with target files")
     args = parser.parse_args()
 
     if not os.path.exists(args.path):
@@ -17,7 +17,10 @@ def get_path():
 
 
 def main():
-    os.chdir(get_path())
+    path = get_path()
+    if path is None:
+        return 1
+    os.chdir(path)
     hash_map = HashMap.HashMap()
     for filename in os.listdir(os.getcwd()):
         size = os.path.getsize(filename)
